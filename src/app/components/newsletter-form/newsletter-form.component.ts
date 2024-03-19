721045f1a7d69a802d7fc5de41d2a9422689f583
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-03-19 17:32:27 -0300

    fimlandpage

--- src/app/components/newsletter-form/newsletter-form.component.ts
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Signal, signal } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NewsletterService } from '../../services/newsletter.service';
 
 @Component({
   selector: 'newsletter-form',
@@ -8,11 +14,13 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './newsletter-form.component.html',
   styleUrl: './newsletter-form.component.scss',
   imports: [BtnPrimaryComponent, ReactiveFormsModule],
+  providers:[NewsletterService]
 })
 export class NewsletterFormComponent {
   newsletterform!: FormGroup;
+  loading = signal(false)
 
-  constructor() {
+  constructor(private service: NewsletterService) {
     this.newsletterform = new FormGroup({
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -20,6 +28,14 @@ export class NewsletterFormComponent {
   }
 
   onSubmit() {
-    console.log(this.newsletterform.value);
+    this.loading.set(true)
+    if(this.newsletterform.valid){
+      this.service.sendDate(this.newsletterform.value.name, this.newsletterform.value.email).subscribe({
+        next: () =>{
+          this.newsletterform.reset()
+          this.loading.set(false)
+        }
+      })
+    }
   }
 }
